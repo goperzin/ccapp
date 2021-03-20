399fd25a32b1cdd202ce809c3ec099fc7c900c80
goperzin <leandrolopes.help@gmail.com>
2021-03-20 02:36:33 -0300

    Primeiro commit app-base command center

---
+++ src/main/java/br/com/itau/ccapp/ccapp/CcappApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.ccapp.ccapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itau/ccapp/ccapp/CcappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.ccapp.ccapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
